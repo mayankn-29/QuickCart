e638726578a2da5d84fb859b7b35449850dda667
mayankn-29 <mayanknishod2001@gmail.com>
2023-09-18 15:07:10 +0530

    create cart app

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const list = [
+    {
+      id: 1,
+      title: "Asus Laptop",
+      price: 62000,
+      img:"./pics/asus.jpg",
+      amount: 1,
+    },
+    {
+      id: 2,
+      title: "Beats Headphones",
+      price: 7500,
+      img: "./pics/beats.jpg",
+      amount: 1,
+    },
+    {
+      id: 3,
+      title: "Apple Macbook",
+      price: 95000,
+      img: "./pics/macbook.jpg",
+      amount: 1,
+    },
+    {
+      id: 4,
+      title: "Samsung Mobile",
+      price: 22000,
+      img: "./pics/samsung.jpg",
+      amount: 1,
+    },
+    {
+      id: 5,
+      title: "1+ Smartphone",
+      price: 35000,
+      img: "./pics/one_plus.jpg",
+      amount: 1,
+    },
+    {
+      id: 6,
+      title: "Apple iphone",
+      price: 60000,
+      img: "./pics/iPhone.jpg",
+      amount: 1,
+    },
+  
+];
+
+export default list;
